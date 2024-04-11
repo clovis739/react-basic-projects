16d9b110c1171dca9a3acc27aed28fb9179a5a7b
clovis739 <clovistumasang82@gmail.com>
2024-04-11 09:05:53 +0100

    Nike shop first commit

---
+++ nike-shop_filtering/src/App.jsx
@@ -0,0 +1,22 @@
+
+
+import './App.css'
+import Nav from './Navigation/Nav'
+import Products from './Products/Products'
+import Recommended from "./Recommended/Recommended"
+import Sidebar from './Sidebar/Sidebar'
+
+function App() {
+  
+
+  return (
+    <>
+    <Sidebar />
+    <Nav />
+    <Recommended  />
+    <Products />  
+    </>
+  )
+}
+
+export default App
